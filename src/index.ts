bf557010f9fc5c9453042773a11c79922d473b23
osawa-koki <koki@osawa.dev>
2024-01-01 00:03:14 +0900

    データの調整まで。

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
-function hello (): void {
-  console.log('Hello World!')
+import createSheet from './createSheet'
+import data from './data'
+
+function main (): void {
+  const sheet = createSheet()
 }
 
 declare let global: any
-global.handler = hello
+global.handler = main
